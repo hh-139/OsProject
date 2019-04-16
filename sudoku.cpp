c6b5e3aa5c6cc096cfb304af2fcc44e05a3efb3c
AbrarFastian <49347290+AbrarFastian@users.noreply.github.com>
2019-04-16 20:49:24 +0500

    Add files via upload

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,40 +1,61 @@
-//hammad hassan
-
 #include<iostream>
-#include<pthread.h>
-#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include <pthread.h>
 using namespace std;
 
-int myarray[9][9];
+#define GRID_ROWS 9
+#define GRID_COLS 9
 
-#define NUM_THREADS 9
+int GRID[GRID_ROWS][GRID_COLS];
+int numbers[9]={1,2,3,4,5,6,7,8,9};
 
-void* grid(void*)
-{
-	for (int i = 0; i < 9; ++i)
+void draw_grid();
+void print_grid();
+void draw_grid();
+int main(){
+
+	//draw_grid();
+	//print_grid();
+
+	for (int m=0;m<GRID_ROWS;m++)
 	{
-		cout<<"                              -------------------------------------"<<endl<<"                              ";
 
-		for (int j = 0; j < 9; ++j)
+		for (int i=0;i<GRID_COLS;i++)
 		{
-			cout<<"| 1 ";
+			int random1=rand()%9;
+			int random2=rand()%9;
+			int temp=numbers[random1];
+			numbers[random1]=numbers[random2];
+			numbers[random2]=temp;
 		}
-		cout<<"|"<<endl;
+
+		for (int i=0;i<9;i++)
+		{
+				GRID[m][i]=numbers[i];
+		}
 	}
-	cout<<"                              -------------------------------------"<<endl;
 
-	pthread_exit(NULL);
-}
-
-int main()
-{
-	pthread_t threadID;
-
-
-	pthread_create(&threadID,NULL,&grid,NULL);
-
-	void* res;
-	pthread_join(threadID,&res);
+	cout<<endl;
+	print_grid();
 
 	return 0;
-}		
+}
+
+void write_grid()
+{
+	int fd=open("output.txt",_WRONLY);
+	
+}
+void print_grid()
+{
+	cout<<"Grid :"<<endl;
+	for (int i=0;i<GRID_ROWS;i++)
+	{
+		for (int j=0;j<GRID_COLS;j++)
+		{
+			cout<<GRID[i][j]<<"    ";
+		}
+		cout<<endl;
+	}	
+}
